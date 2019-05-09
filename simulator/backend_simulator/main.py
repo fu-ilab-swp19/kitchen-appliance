190c3dbc66a6e0b7de970333d86f2376bba09dfc
Sebastian Oltmanns <sebastian.oltmanns.developer@gmail.com>
2019-05-09 14:07:13 +0200

    backend simulator can request the sensor simulator and display the values

--- simulator/backend_simulator/main.py
+++ simulator/backend_simulator/main.py
@@ -1,22 +1,55 @@
 import socket
 import sys
+import matplotlib.pyplot as plt
+import time
+import numpy as np
+import datetime
 
-def main():
-	
-	HOST = '::1'
-	PORT = int(sys.argv[1])
+def getSensorData(host, port):
 	
 	sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 	
+	received = None
+	
 	try: 
-		sock.connect((HOST, PORT))
-		sock.sendall(bytes('TEST\n', 'utf-8'))
+		sock.connect((host, port))
+		sock.sendall(bytes('\n', 'utf-8'))
 		
-		received = str(sock.recv(1024), 'utf-8')
+		received = float(str(sock.recv(1024), 'utf-8'))
 	finally:
 		sock.close()
 		
-	print('received:', received)
+	if received is None:
+		raise
+	
+	return received
+
+def main():
+	
+	host = '::1'
+	port = int(sys.argv[1])
+	
+	update_interval = float(sys.argv[2])
+	
+	x_data = np.array([])
+	x_ctr = 0
+	y_data = np.array([])
+	
+	fig = plt.figure(figsize=(13,6))
+	ax = fig.add_subplot(111)
+	ax.plot(x_data, y_data)
+	
+	while(True):
+		try:
+			d = getSensorData(host, port)
+			y_data = np.append(y_data, d)[-500:]
+			x_data = np.append(x_data, datetime.datetime.now())[-500:]
+			ax.clear()
+			ax.plot(x_data,y_data)
+			plt.pause(0.1)
+		except:
+			pass
+		time.sleep(update_interval)
 	
 if __name__ == "__main__":
 	
